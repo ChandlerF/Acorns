f15284c3f8f1ff229728e2cd647dcd07c03326ee
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2023-03-21 17:23:57 -0500

    Initial

---
+++ Assets/PlayerNetwork.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    private Rigidbody2D _rb;
+
+    private float _horizontal;
+    private float _vertical;
+
+    [SerializeField]  private float _speed = 8.5f;
+    [SerializeField] private GameObject _building1;
+
+    void Start()
+    {
+        if(!IsOwner) this.enabled = false;
+
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnPos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+
+
+            if (!IsServer)
+                SpawnBuildingServerRpc(spawnPos);
+            else
+                SpawnBuildingClient(spawnPos);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_horizontal * _speed, _vertical * _speed);
+    }
+
+
+
+    public void SpawnBuildingClient(Vector3 pos)
+    {
+        GameObject spawnedBuilding = Instantiate(_building1, pos, Quaternion.identity);
+        spawnedBuilding.GetComponent<NetworkObject>().Spawn(true);
+    }
+
+
+    [ServerRpc]
+    public void SpawnBuildingServerRpc(Vector3 pos)
+    {
+        SpawnBuildingClient(pos);
+    }
+}
